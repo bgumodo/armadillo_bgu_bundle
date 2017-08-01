a2d7f99889bff730cea153a5e217dc62e92732c1
shaigivati <shaigiv@post.bgu.ac.il>
2017-08-01 17:36:29 +0300

    temp2

--- find_obj_temp/src/find_object.cpp
+++ find_obj_temp/src/find_object.cpp
@@ -21,19 +21,21 @@ void fail(){
 }
 
 bool srv_trigger(object_identification::find_obj::Request &req, object_identification::find_obj::Response &res){
+    ROS_INFO("Triggered!");
     // get image and point-cloud
     ros::Duration d(10.0);
     
     // no message filter here, assuming robot dosn't move!
-    boost::shared_ptr<const sensor_msgs::Image> rgb = ros::topic::waitForMessage<sensor_msgs::Image>("/kinect2/qhd/image_color", d);
-    boost::shared_ptr<const sensor_msgs::PointCloud2> pcl = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/kinect2/qhd/points", d);
-    if(!rgb || !pcl){
+    boost::shared_ptr<const sensor_msgs::Image> rgb_msg = ros::topic::waitForMessage<sensor_msgs::Image>("/kinect2/qhd/image_color", d);
+    boost::shared_ptr<const sensor_msgs::PointCloud2> pcl_msg = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/kinect2/qhd/points", d);
+    if(!rgb_msg || !pcl_msg){
         fail();
         return true;
     }
 
     // find object in rgb image
-    
+    cv::Mat rgb_img = cv_bridge::toCvCopy(rgb_msg)->image;
+    cv::imshow("sample", rgb_img);
 
     // publish location to topic and ps_interface
     std::string name = req.name;
@@ -52,6 +54,7 @@ int main(int argc, char **argv){
     ros::ServiceServer srv = nh.advertiseService("find_object", srv_trigger);
     pub = nh.advertise<object_identification::find_results>("find_results", 10);
     
+    ROS_INFO("temp find_object is ready.");
     ros::spin();
 
     return 0;
