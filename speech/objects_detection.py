01f0df1d1d356b8effef93adde35bad84ed63a5f
Dan <dan.z.schulman@gmail.com>
2018-01-02 17:42:59 +0200

    objects detection

--- speech/objects_detection.py
+++ speech/objects_detection.py
@@ -13,11 +13,33 @@ def processRequest(data):
     headers = dict()
     headers['Content-Type'] = 'application/octet-stream'
 
-    json = None
+    response = requests.request('post', _url, data=data, headers=headers, params=params)
 
-    response = requests.request('post', _url, json=json, data=data, headers=headers, params=params)
+    string_io = StringIO(response.content)
+    json_result = json.load(string_io)
 
-    return response.content
+    classes_dict = {}
+    for object in json_result['class_names']:
+        if object not in classes_dict:
+            classes_dict[object] = 1
+        else:
+            classes_dict[object] += 1
+
+    objects_string = ''
+    for i, object in enumerate(classes_dict):
+        if classes_dict[object] == 1:
+            objects_string += str(classes_dict[object]) + ' ' + object.split('|')[0]
+        else:
+            objects_string += str(classes_dict[object]) + ' ' + object.split('|')[1]
+
+        if i < len(classes_dict) - 2:
+            objects_string += ', '
+        elif i == len(classes_dict) - 2:
+            objects_string += ' and '
+
+    result = {'result': json_result, 'objects_string': objects_string}
+
+    return result
 
 
 def main():
@@ -28,10 +50,9 @@ def main():
 
     result = processRequest(data)
 
-    if result is not None:
-        string_io = StringIO(result)
-        result_dict = json.load(string_io)
-        print(result_dict)
+    print(result)
+
+
 
 
 if __name__ == '__main__':
